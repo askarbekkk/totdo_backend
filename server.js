d02db6c9f784d07684a314d6776054c3a6e07e01
Askar <askarkanatbekovic@gmail.com>
2021-04-23 15:59:52 +0400

    added delete

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ function getData(category) {
     return JSON.parse(fs.readFileSync(`./tasks/${category}.json`, 'utf8'))
 }
 
-server.get('/api/tasks/:category', async (req, res) => {
+server.get('/api/v1/tasks/:category', async (req, res) => {
 //
     const {category} = req.params
     console.log(req.params)
@@ -47,9 +47,9 @@ server.patch('/api/v1/tasks/:category/:id', (req, res) => {
     res.json({status: 'Updated'})
 })
 
-server.get('*', (req, res) => {
-    res.status(404).send('Not found')
-})
+// server.get('*', (req, res) => {
+//     res.status(404).send('Not found')
+// })
 
 server.delete('/api/v1/tasks/:category/', (req, res) => {
     const {category} = req.params
@@ -58,14 +58,24 @@ server.delete('/api/v1/tasks/:category/', (req, res) => {
 
 })
 
+server.delete('/api/v1/tasks/:category/:id', (req, res) => {
 
-// server.put('/api/tasks/:category/', (req, res) => {
-//     const {category} = req.params
-//     let task = getData(category)
-//     console.log(req.params)
-//     fs.writeFileSync(`./tasks/${category}.json`, JSON.stringify(task, null, 2))
-//     res.json({status: 'tasks were updated'})
-// })
+    const {category,id} = req.params
+    let tasks = getData(category,id)
+    let newTasks = tasks.filter(el => el.taskId !== id)
+    fs.writeFileSync(`./tasks/${category}.json/`, JSON.stringify(newTasks,null,2))
+    res.json({status: 'task is deleted'})
+
+})
+
+
+server.put('/api/v1/tasks/:category/', (req, res) => {
+    const {category} = req.params
+    let task = getData(category)
+    console.log(req.params)
+    fs.writeFileSync(`./tasks/${category}.json`, JSON.stringify(task, null, 2))
+    res.json({status: 'tasks were updated'})
+})
 
 server.listen(8000, () => console.log('Server is started'))
 
@@ -77,3 +87,12 @@ const withoutUnderline = (array) => {
         return {taskId: el.taskId, title: el.title, status: el.status}
     })
 }
+
+server.get('/api/v1/tasks/:category/:id', async (req, res) => {
+//
+    const {category} = req.params
+    console.log(req.params)
+    let task = withoutUnderline(await getData(category))
+    res.json(task)
+})
+
