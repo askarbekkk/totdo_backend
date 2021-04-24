2340c7273e5ff2f4f709f0ab8b7570cf2eac9bc4
Askar <askarkanatbekovic@gmail.com>
2021-04-25 02:28:57 +0400

    added file

--- server.js
+++ server.js
@@ -7,11 +7,15 @@ const fs = require("fs");
 const server = express()
 
 server.use(express.json())
-const {readFileSync, writeFileSync, stat, unlink} = require('fs')
+const {readFileSync, writeFileSync, writeFile, stat, unlink} = require('fs')
 
 function getData(category) {
     console.log(__dirname)
-    return JSON.parse(fs.readFileSync(`./tasks/${category}.json`, 'utf8'))
+    try {
+        return JSON.parse(fs.readFileSync(`./tasks/${category}.json`, 'utf8'))
+    } catch (err) {
+        return []
+    }
 }
 
 server.get('/api/v1/tasks/:category', async (req, res) => {
@@ -24,7 +28,7 @@ server.get('/api/v1/tasks/:category', async (req, res) => {
 
 server.post('/api/v1/tasks/:category', async (req, res) => {
     const {category} = req.params
-    let tasks = JSON.parse(fs.readFileSync(`./tasks/${category}.json`, 'utf8'))
+    let tasks = getData(category)
     let newTask = {
         taskId: uniqid(),
         title: req.body.title,
@@ -34,13 +38,17 @@ server.post('/api/v1/tasks/:category', async (req, res) => {
         _deletedAt: null
     }
     let newTasks = [...tasks, newTask]
-    fs.writeFileSync(`./tasks/${category}.json`, JSON.stringify(newTasks, 1, 2))
+    fs.writeFile(`./tasks/${category}.json`, JSON.stringify(newTasks, null, 2), function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+
     res.json({status: 'added successfully'})
 })
 
 server.patch('/api/v1/tasks/:category/:id', (req, res) => {
     const {category} = req.params
-    let task = getData(category)
+    let task = getData(category )
     let id = req.params.id
     let newTask = task.map(el => el.taskId === id ? {...el, ...req.body} : el)
     fs.writeFileSync(`./tasks/${category}.json`, JSON.stringify(newTask, null, 2))
@@ -71,7 +79,7 @@ server.delete('/api/v1/tasks/:category/:id', (req, res) => {
 
 server.put('/api/v1/tasks/:category/', (req, res) => {
     const {category} = req.params
-    let task = getData(category)
+    let task = getData(category )
     console.log(req.params)
     fs.writeFileSync(`./tasks/${category}.json`, JSON.stringify(task, null, 2))
     res.json({status: 'tasks were updated'})
